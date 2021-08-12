f3885941fb771b644cd37ed643a6d07445bad5d7
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-08-12 07:42:27 +0300

    Fix plugin loading issues

--- packages/core/src/plugins/hiddenSiteNav.js
+++ packages/core/src/plugins/hiddenSiteNav.js
@@ -3,7 +3,7 @@ const cheerio = module.parent.require('cheerio');
 
 module.exports = {
     processNode: (pluginContext, node) => {
-        if (node.attribs.slot === 'brand') {
+        if (node.attribs.hasOwnProperty('#brand')) {
             cheerio(node)
                 .children('.navbar-brand')
                 .removeAttr('slot')
@@ -12,37 +12,37 @@ module.exports = {
                     '<button '
                     + 'href="#" '
                     + 'id="site-nav-show-button"'
-                    + `class="fa fa-bars btn btn-${node.attribs.type}"`
+                    + `class="fa fa-bars btn btn-dark"`
                     + '></button>'
                 )
                 .before(
                     '<button '
                     + 'href="#" '
                     + 'id="site-nav-hide-button"'
-                    + `class="fa fa-chevron-left btn btn-${node.attribs.type}" `
+                    + `class="fa fa-chevron-left btn btn-dark" `
                     + 'hidden'
                     + '></button>'
-                )
+                );
         }
 
         if (node.attribs.id === 'site-nav') {
             cheerio(node).attr('hidden', 'hidden');
         }
     },
-    postRender: (pluginContext, frontMatter, content) => {
-        const $ = cheerio.load(content, { xmlMode: false });
-        $('#site-nav-show-button').click(() => {
-            $('#site-nav').removeAttr('hidden');
-            $('#site-nav-hide-button').removeAttr('hidden');
-            $('#site-nav-show-button').attr('hidden', 'hidden');
-        });
-
-        $('#site-nav-hide-button').click(() => {
-            $('#site-nav').attr('hidden', 'hidden');
-            $('#site-nav-hide-button').attr('hidden', 'hidden');
-            $('#site-nav-show-button').removeAttr('hidden');
-        });
-
-        return $.html();
-    }
+    getScripts: (pluginContext, frontMatter, content) => [
+        '<script>'
+        + "$('#site-nav-show-button').click(() => {"
+        + "    $('#site-nav').removeAttr('hidden');"
+        + "    $('#site-nav-hide-button').removeAttr('hidden');"
+        + "    $('#site-nav-show-button').attr('hidden', 'hidden');"
+        + "});"
+        + '</script>',
+        '<script>'
+        + "$('#site-nav-hide-button').click(() => {"
+        + "    $('#site-nav').attr('hidden', 'hidden');"
+        + "    $('#site-nav-hide-button').attr('hidden', 'hidden');"
+        + "    $('#site-nav-show-button').removeAttr('hidden')"
+        + "});"
+        + '</script>'
+    ]
 }
