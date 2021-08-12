35ede5c880aa09507585c15eb9b51653d23ab229
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-08-12 08:02:00 +0300

    Enable site nav buttons to adopt the navbar color

--- packages/core/src/plugins/hiddenSiteNav.js
+++ packages/core/src/plugins/hiddenSiteNav.js
@@ -3,6 +3,10 @@ const cheerio = module.parent.require('cheerio');
 
 module.exports = {
     processNode: (pluginContext, node) => {
+        if (node.name === 'navbar') {
+            pluginContext.navbarTheme = node.attribs.type;
+        }
+
         if (node.attribs.hasOwnProperty('#brand')) {
             cheerio(node)
                 .children('.navbar-brand')
@@ -12,14 +16,14 @@ module.exports = {
                     '<button '
                     + 'href="#" '
                     + 'id="site-nav-show-button"'
-                    + `class="fa fa-bars btn btn-dark"`
+                    + `class="fa fa-bars btn btn-${pluginContext.navbarTheme}"`
                     + '></button>'
                 )
                 .before(
                     '<button '
                     + 'href="#" '
                     + 'id="site-nav-hide-button"'
-                    + `class="fa fa-chevron-left btn btn-dark" `
+                    + `class="fa fa-chevron-left btn btn-${pluginContext.navbarTheme}" `
                     + 'hidden'
                     + '></button>'
                 );
