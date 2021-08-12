cd690120fff283f32124f113bcdd03537e0c6ba8
ALZAHRANI-PC\riyad <riyadh-h@outlook.sa>
2021-08-12 14:19:08 +0300

    Adjust code styling for hiddenSiteNav.js

--- packages/core/src/plugins/hiddenSiteNav.js
+++ packages/core/src/plugins/hiddenSiteNav.js
@@ -2,54 +2,41 @@
 const cheerio = module.parent.require('cheerio');
 
 module.exports = {
-    processNode: (pluginContext, node) => {
-        if (node.name === 'navbar') {
-            pluginContext.navbarTheme = node.attribs.type;
-        }
+  processNode: (pluginContext, node) => {
+    if (node.name === 'navbar') {
+      pluginContext.navbarTheme = node.attribs.type;
+    }
 
-        if (node.attribs.hasOwnProperty('#brand')) {
-            cheerio(node)
-                .children('.navbar-brand')
-                .removeAttr('slot')
-                .wrap('<div slot="brand"></div>')
-                .before(
-                    '<button '
-                    + 'href="#" '
-                    + 'id="site-nav-show-button"'
-                    + `class="fa fa-bars btn btn-${pluginContext.navbarTheme}"`
-                    + '></button>'
-                )
-                .before(
-                    '<button '
-                    + 'href="#" '
-                    + 'id="site-nav-hide-button"'
-                    + `class="fa fa-chevron-left btn btn-${pluginContext.navbarTheme}" `
-                    + 'hidden'
-                    + '></button>'
-                );
-        }
+    if (node.attribs.hasOwnProperty('#brand')) {
+      cheerio(node)
+        .children('.navbar-brand')
+        .removeAttr('slot')
+        .wrap('<div slot="brand"></div>')
+        .before(
+          '<button href="#" id="site-nav-show-button" '
+          + `class="fa fa-bars btn btn-${pluginContext.navbarTheme}"></button>`
+        )
+        .before(
+          '<button href="#" id="site-nav-hide-button" '
+          + `class="fa fa-chevron-left btn btn-${pluginContext.navbarTheme}" hidden></button>`
+        );
+    }
 
-        if (node.attribs.id === 'site-nav') {
-            cheerio(node).attr('hidden', 'hidden');
-        }
-    },
-    getLinks: (pluginContext, frontMatter, content) => [
-        '<link rel="stylesheet" href="hiddenSiteNav.css">'
-    ],
-    getScripts: (pluginContext, frontMatter, content) => [
-        '<script>'
-        + "$('#site-nav-show-button').click(() => {"
-        + "    $('#site-nav').removeAttr('hidden');"
-        + "    $('#site-nav-hide-button').removeAttr('hidden');"
-        + "    $('#site-nav-show-button').attr('hidden', 'hidden');"
-        + "});"
-        + '</script>',
-        '<script>'
-        + "$('#site-nav-hide-button').click(() => {"
-        + "    $('#site-nav').attr('hidden', 'hidden');"
-        + "    $('#site-nav-hide-button').attr('hidden', 'hidden');"
-        + "    $('#site-nav-show-button').removeAttr('hidden')"
-        + "});"
-        + '</script>'
-    ]
+    if (node.attribs.id === 'site-nav') {
+      cheerio(node).attr('hidden', 'hidden');
+    }
+  },
+  getLinks: (pluginContext, frontMatter, content) => [
+    '<link rel="stylesheet" href="hiddenSiteNav.css">'
+  ],
+  getScripts: (pluginContext, frontMatter, content) => [
+    '<script> $("#site-nav-show-button").click(() => {'
+    + '$("#site-nav").removeAttr("hidden");'
+    + '$("#site-nav-hide-button").removeAttr("hidden");'
+    + '$("#site-nav-show-button").attr("hidden", "hidden");});'
+    + '$("#site-nav-hide-button").click(() => {'
+    + '$("#site-nav").attr("hidden", "hidden");'
+    + '$("#site-nav-hide-button").attr("hidden", "hidden");'
+    + '$("#site-nav-show-button").removeAttr("hidden")}); </script>'
+  ]
 }
